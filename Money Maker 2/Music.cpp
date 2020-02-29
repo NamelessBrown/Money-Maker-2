a4a1491bf0cff7d7baeb45fe5bb7a0a7fc82df41
Nameless <tyrebrown873@gmail.com>
2020-02-29 16:33:16 -0500

    Added More Music

--- Money Maker 2/Music.cpp
+++ Money Maker 2/Music.cpp
@@ -1,7 +1,7 @@
 #include "Music.h"
 
 Music::Music()
-	:mRd(), mMt(mRd()), distribution(1, 10), mWhatFile(distribution(mMt))
+	:mRd(), mMt(mRd()), distribution(1, 14), mWhatFile(distribution(mMt))
 {
 	mSongs.emplace_back("Music/BLISSbyKENARAI.ogg");
 	mSongs.emplace_back("Music/HamptontheHamsterTheHamsterdanceSong.ogg");
@@ -13,6 +13,10 @@ Music::Music()
 	mSongs.emplace_back("Music/SunflowerDanceParty.ogg");
 	mSongs.emplace_back("Music/ShowYourMoves.ogg");
 	mSongs.emplace_back("Music/TheDuckSong.ogg");
+	mSongs.emplace_back("Music/[Future Bass]-Subtact-Restart[Monstercat Release].ogg");
+	mSongs.emplace_back("Music/TheFatRat-MAYDAYfeat.Laura Brehm.ogg");
+	mSongs.emplace_back("Music/TheFatRat-Monody(feat.Laura Brehm).ogg");
+	mSongs.emplace_back("Music/TheFatRat-FlyAwayfeat.Anjulie.ogg");
 }
 
 void Music::Play()
